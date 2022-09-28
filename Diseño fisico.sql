21eda6c383a50963dc4895b5171be4f393b2cf18
Davidrk31 <89051979+Davidrk31@users.noreply.github.com>
2022-09-28 15:38:30 -0500

    Create Diseño fisico.sql

---
+++ Diseño fisico.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE proyecto;
+
+USE proyecto;
+
+CREATE TABLE IF NOT EXISTS Clientes ( 
+	cliente_id INT NOT NULL PRIMARY KEY,
+	nombre VARCHAR(50),
+	direccion VARCHAR(50),
+    pais VARCHAR(10),
+    telefono VARCHAR(15),
+    ciudad VARCHAR(15),
+    Password VARCHAR(30),
+    EstadoLog VARCHAR(1)
+	);
+
+CREATE TABLE IF NOT EXISTS Ordenes ( 
+	Orden_id INT NOT NULL PRIMARY KEY,
+	Cliente_id VARCHAR(50),
+	direccion_entrega VARCHAR(50),
+    Fecha VARCHAR(10),
+    Cantidad VARCHAR(5),
+    Estado VARCHAR(15)
+	);
+    
+CREATE TABLE IF NOT EXISTS Producto ( 
+	producto_id INT NOT NULL PRIMARY KEY,
+	nombre VARCHAR(50),
+	categoria VARCHAR(20),
+    sku VARCHAR(10),
+    Precio VARCHAR(15),
+    stock VARCHAR(5)
+	);
+    
+CREATE TABLE IF NOT EXISTS Vendedor ( 
+	vendedor_id INT NOT NULL PRIMARY KEY,
+	nombre VARCHAR(50),
+	direccion VARCHAR(50),
+    email VARCHAR(15)
+	);
+    
+CREATE TABLE IF NOT EXISTS Producto_vendedor ( 
+	id INT NOT NULL PRIMARY KEY,
+	producto_id VARCHAR(10),
+	vendedor_id VARCHAR(10)
+	);
+    
+    CREATE TABLE IF NOT EXISTS Producto_categoria ( 
+	id INT NOT NULL PRIMARY KEY,
+	producto_id VARCHAR(10),
+	categoria_id VARCHAR(10)
+	);
