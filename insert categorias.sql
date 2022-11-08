dca3ff313de88ffb6346ce2d888f8be8c8de67a7
Davidrk31 <89051979+Davidrk31@users.noreply.github.com>
2022-11-07 19:11:22 -0500

    Create insert categorias.sql

---
+++ insert categorias.sql
@@ -0,0 +1,11 @@
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (P4,'Celulares','Celulares,Accesorios para celular y cargadores',);
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (P4,'Computadores','Desde computadores de escritorio hasta computadores gamer',);
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (P4,'Electrodomesticos','Los mejores productos para el hogar desde neveras,microondas,etc.',);
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (P4,'Televisores','Televisores ultimo modelo',);
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (P4,'Moda','Ropa para todas las personas',);
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (P4,'Navidad','Productos para temporada navideña',);
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (P4,'Tecnologia','Todos los aparatos tecnologicos deseados',);
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (P4,'Deportes','Accesorios y aparatos para el deporte',);
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (P4,'Belleza','Productos para belleza y el cuidado personal',);
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (P4,'Inmuebles','Muebles para tu hogar',);
+
