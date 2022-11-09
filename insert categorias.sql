6d2bfd151b2c99d8f7b6f20efc831de861d5780a
LuisFelipeU38 <83362726+LuisFelipeU38@users.noreply.github.com>
2022-11-09 16:03:39 -0500

    Update insert categorias.sql

--- insert categorias.sql
+++ insert categorias.sql
@@ -1,10 +1,11 @@
-INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (001,'Celulares','Celulares,Accesorios para celular y cargadores',);
-INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (002,'Computadores','Desde computadores de escritorio hasta computadores gamer',);
-INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (003,'Electrodomesticos','Los mejores productos para el hogar desde neveras,microondas,etc.',);
-INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (004,'Televisores','Televisores ultimo modelo',);
-INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (005,'Moda','Ropa para todas las personas',);
-INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (006,'Navidad','Productos para temporada navideña',);
-INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (007,'Tecnologia','Todos los aparatos tecnologicos deseados',);
-INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (008,'Deportes','Accesorios y aparatos para el deporte',);
-INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (009,'Belleza','Productos para belleza y el cuidado personal',);
-INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (010,'Inmuebles','Muebles para tu hogar',);
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (001,'Celulares','Celulares,Accesorios para celular y cargadores');
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (002,'Computadores','Desde computadores de escritorio hasta computadores gamer');
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (003,'Electrodomesticos','Los mejores productos para el hogar desde neveras,microondas,etc.');
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (004,'Televisores','Televisores ultimo modelo');
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (005,'Moda','Ropa para todas las personas');
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (006,'Navidad','Productos para temporada navideña');
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (007,'Tecnologia','Todos los aparatos tecnologicos deseados');
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (008,'Deportes','Accesorios y aparatos para el deporte');
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (009,'Belleza','Productos para belleza y el cuidado personal');
+INSERT INTO categorias(categoria_id,nombre,descripcion) VALUES (010,'Inmuebles','Muebles para tu hogar');
+
