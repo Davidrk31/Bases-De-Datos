149a7ed324282ee4c3a2571d3c30665c5e6c2219
LuisFelipeU38 <83362726+LuisFelipeU38@users.noreply.github.com>
2022-11-08 19:15:31 -0500

    Update Diseño fisico.sql

--- Diseño fisico.sql
+++ Diseño fisico.sql
@@ -8,11 +8,11 @@ CREATE TABLE IF NOT EXISTS Clientes (
 	cliente_id INT NOT NULL PRIMARY KEY,
 	nombre VARCHAR(50),
 	direccion VARCHAR(50),
-    pais VARCHAR(10),
-    telefono VARCHAR(15),
+    pais VARCHAR(50),
+    telefono INT,
     ciudad VARCHAR(15),
-    Password VARCHAR(30),
-    EstadoLog VARCHAR(1)
+    Password_ VARCHAR(30),
+    EstadoLog INT
 	);
 
 CREATE TABLE IF NOT EXISTS Ordenes ( 
@@ -24,30 +24,28 @@ CREATE TABLE IF NOT EXISTS Ordenes (
     Estado VARCHAR(15)
 	);
     
-CREATE TABLE IF NOT EXISTS Producto ( 
+CREATE TABLE IF NOT EXISTS Productos ( 
 	producto_id INT NOT NULL PRIMARY KEY,
 	nombre VARCHAR(50),
-	categoria VARCHAR(20),
-    sku VARCHAR(10),
-    Precio VARCHAR(15),
-    stock VARCHAR(5)
-	);
-    
-CREATE TABLE IF NOT EXISTS Vendedor ( 
-	vendedor_id INT NOT NULL PRIMARY KEY,
-	nombre VARCHAR(50),
-	direccion VARCHAR(50),
-    email VARCHAR(15)
-	);
-    
-CREATE TABLE IF NOT EXISTS Producto_vendedor ( 
-	id INT NOT NULL PRIMARY KEY,
-	producto_id VARCHAR(10),
-	vendedor_id VARCHAR(10)
+    sku INT,
+    Precio INT,
+    stock INT
 	);
     
     CREATE TABLE IF NOT EXISTS Producto_categoria ( 
-	id INT NOT NULL PRIMARY KEY,
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	producto_id VARCHAR(10),
 	categoria_id VARCHAR(10)
 	);
+    
+    CREATE TABLE IF NOT EXISTS Orden_productos ( 
+	orden_id INT NOT NULL PRIMARY KEY,
+	producto_id VARCHAR(10),
+	precio NUMERIC(5,0)
+	);
+    
+    CREATE TABLE IF NOT EXISTS Categorias ( 
+	categoria_id INT NOT NULL PRIMARY KEY,
+	nombre VARCHAR(50),
+	descripcion VARCHAR(80)
+   );
